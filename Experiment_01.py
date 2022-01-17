0b049823362e351881a5febc45a9c6e399726737
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-01-17 11:50:34 +0530

    main

--- Experiment_01.py
+++ Experiment_01.py
@@ -1,8 +1,16 @@
 from csv import reader
 file=open("finds.csv", 'r')
 data=reader(file)
-rows=[]
+H=None
 for row in data:
-    rows.append(row)
-for _ in rows:
-    print(_)
+    if row[-1]=='Yes':
+        if H==None:
+            H=row
+        else:
+            for i in range(len(row)-1):
+                if H[i]!=row[i]:
+                    H[i]='?'
+    else:
+        continue
+
+print(H[:-1])
