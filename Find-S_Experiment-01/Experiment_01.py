fcb75cdfd4a58d8f2ad58bd4af0acaabf18e57e1
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-01-17 11:57:02 +0530

    main

--- Find-S_Experiment-01/Experiment_01.py
+++ Find-S_Experiment-01/Experiment_01.py
@@ -11,8 +11,7 @@ for row in data:
             for i in range(len(row)-1):
                 if H[i]!=row[i]:
                     H[i]='?'
-    else:
-        continue
     print(f"Training Example number {n+1} hypothesis: ",H[:-1])
+    n=n+1
 
 print("\nMaxymal Hypothesis:", H[:-1])
