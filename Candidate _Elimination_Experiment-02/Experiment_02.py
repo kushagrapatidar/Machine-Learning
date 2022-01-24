506fcbc4476938e44eb9b81f08047bc7ff63e723
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-01-24 22:29:32 +0530

    main

--- Candidate _Elimination_Experiment-02/Experiment_02.py
+++ Candidate _Elimination_Experiment-02/Experiment_02.py
@@ -2,16 +2,36 @@ from csv import reader
 file=open("finds.csv", 'r')
 data=reader(file)
 H=None
+G=[]
 n=0
 for row in data:
     if row[-1]=='Yes':
         if H==None:
-            H=row
+            H=row[:-1]
         else:
             for i in range(len(row)-1):
                 if H[i]!=row[i]:
                     H[i]='?'
-    print(f"Training Example number {n+1} hypothesis: ",H[:-1])
+    elif row[-1]=='No':
+        for i in range(len(H)):
+            h=['?']*len(H)
+            if H[i]!=row[i]:
+                h[i]=H[i]
+            G.append(h)
     n=n+1
 
-print("\nMaxymal Hypothesis:", H[:-1])
+for g in G:
+    for i in range(len(g)):
+        if H[i]==g[i]:
+            continue
+        else:
+            g[i]='?'
+
+while True:
+    if ['?']*len(H) in G:
+        G.remove(['?']*len(H))
+    else:
+        break
+
+print(G)
+print("\n\nMaxymal Hypothesis:", H)
