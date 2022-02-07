566add331b5ea82e490efd1bb5977907f4360fa4
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-02-07 22:39:17 +0530

    main

--- Decision Tree/Experiment_03.py
+++ Decision Tree/Experiment_03.py
@@ -1,7 +1,7 @@
 class node:
     data=None
     weights=list()
-    next_attributes=dict()
+    next_attributes=None
     def _init_(self,head):
         self.data=head
 
@@ -10,9 +10,8 @@ def calculate_entropy(attribute):
     return 0
 def calculate_gain(attribute,next_attribute):
     return 0
-from csv import reader
 
-from numpy import array
+from csv import reader
 file=open("decisionTree.csv", 'r')
 Data=reader(file)
 Heads=None
@@ -34,10 +33,9 @@ for i in range(len(Heads)):
     Heads[i].weights=Weights
 [print(Heads[_].data,": ",Heads[_].weights) for _ in range(len(Heads))]
 # [print(row) for row in Data]
-print(len(Heads))
 for x in range(len(Heads)):
-    print(x)
+    Heads[x].next_attributes=dict()
     for weight in Heads[x].weights:
         if weight not in Heads[x].next_attributes.keys():
             Heads[x].next_attributes[weight]=None
-        print(Heads[x].data,Heads[x].next_attributes.keys())
+    print(Heads[x].data,Heads[x].next_attributes.keys())
